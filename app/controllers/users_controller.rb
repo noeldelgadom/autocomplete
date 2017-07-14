e7f046135802086280c37b6d6ff337aee9ebf7f2
Noel Delgado <noeldelgadom@myhost.local>
2017-07-14 15:39:44 -0500

    search for users working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    if params[:term]
+      @users = User.search_by_full_name(params[:term]).with_pg_search_highlight
+    else
+      @users = User.all
+    end
   end
 end
